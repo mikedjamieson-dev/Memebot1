4d0ed124c04c126fd16e6ff68f0eb4820fd57232
mikedjamieson-dev <mike.d.jamieson@gmail.com>
2026-05-03 19:41:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
